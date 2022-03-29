fac913075a28a3ea73a16585134bfba7601276ce
loganylwu <loganylwu@tencent.com>
2022-03-29 22:53:33 +0800

    [+] 添加事件通信

--- events/main.js
+++ events/main.js
@@ -1,15 +1,18 @@
 // main.js
 
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron')
+const {app, BrowserWindow} = require('electron')
 const path = require('path')
 
+require('./main.listen.js')
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
       preload: path.join(__dirname, 'preload.js')
     }
   })
